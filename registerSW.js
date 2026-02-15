e6d8aeefe1b396db400180ed665ff98d5914e6c2
uzmacbook <rejabovabbos605@gmail.com>
2026-02-15 09:17:22 +0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/767-kutubxona/sw.js', { scope: '/767-kutubxona/' })})}
